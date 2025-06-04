87a1643f9ae1fea3a1c2fd9b89a8889c88d68de2
zane <czl970721@gmail.com>
2025-06-03 22:27:42 -0400

    more easy...

---
+++ src/00268-easy-if.ts
@@ -0,0 +1,12 @@
+import type { Equal, Expect } from '../utils'
+
+type If<Iden extends boolean, A, B> = Iden extends true ? A : B;
+
+type cases = [
+  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<If<false, 'a', 2>, 2>>,
+  Expect<Equal<If<boolean, 'a', 2>, 'a' | 2>>,
+]
+
+// @ts-expect-error
+type error = If<null, 'a', 'b'>
---
+++ src/00898-easy-includes.ts
@@ -0,0 +1,30 @@
+import type { Equal, Expect } from '../utils'
+
+type IsEqual<T, U> =   
+    (<G>() => G extends T ? 1 : 2) extends
+    (<G>() => G extends U ? 1 : 2) ? true : false;
+
+type Includes<T extends unknown[], U> = IsEqual<T[0], U> extends true ? true : (
+    T extends [T[0], ...(infer Rest)] ? ( Rest extends [unknown, ...unknown[]] ? Includes<Rest, U> : false) : false
+);
+
+type A = IsEqual<null, undefined>;
+
+type cases = [
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Includes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[1], 1 | 2>, false>>,
+  Expect<Equal<Includes<[1 | 2], 1>, false>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>,
+]
